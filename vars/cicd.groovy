aa0d98cd212444da0327cd19e11c4e9cb5bad615
gtarun2311 <grktarun23@gmail.com>
2025-06-02 15:54:12 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,1 +1,16 @@
-
+def gitdownload(repo)
+{
+git "https://github.com/IntelliqDevops/${repo}.git"
+}
+def buitartifact()
+{
+  git "mvn package"
+}
+def deploytomcat(jobname,ip,context)
+{
+sh 'scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war'
+}
+def runselenium(jobname)
+{
+  sh 'java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar
+}
