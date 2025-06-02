1520cf83aa37e69a7732622b9db251b16a3a7997
gtarun2311 <grktarun23@gmail.com>
2025-06-02 20:39:29 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -8,9 +8,9 @@ def builtartifact()
 }
 def deploytomcat(jobname,ip,context)
 {
-sh 'scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war'
+sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
 }
 def runselenium(jobname)
 {
-  sh 'java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar'
+  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
 }
