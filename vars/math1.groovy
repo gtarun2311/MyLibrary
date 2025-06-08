2f59973bade755c4729ce83afa301c233af3d737
gtarun2311 <grktarun23@gmail.com>
2025-06-08 14:19:04 +0530

    Update math1.groovy

--- vars/math1.groovy
+++ vars/math1.groovy
@@ -1,8 +1,10 @@
 def sum (a,b)
 {
   echo" The sum of ${a} and ${b} is ${a+b}
-}
-def mul (a+b,b)
-{
   echo" The multiplication of ${a+b} and ${b} is ${a+b*b}
+}
+def mul (c,d)
+{
+   echo" The mul of ${c} and ${d} is ${c*d}
+  echo" The multiplication of ${c*d} and ${d} is ${c*d*d}
 }
