b665a80bb78771f5a69bf67f95e931dcbd1e1ebd
gtarun2311 <grktarun23@gmail.com>
2025-06-08 16:03:37 +0530

    Create cicd1.groovy

---
+++ vars/cicd1.groovy
@@ -0,0 +1,16 @@
+def download (repo)
+{
+  git 'https://github.com/IntelliqDevops/&{repo}.git'
+}
+def build ()
+{
+  sh 'mvn package'
+}
+def deployment (job,ip,name)
+{
+  sh 'scp /var/lib/jenkins/workspace/${job}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${name}.war'
+}
+def test (name)
+{
+  sh 'java -jar /var/lib/jenkins/workspace/${name}/testing.jar'
+}
